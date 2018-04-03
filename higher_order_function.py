5d95684cee27315465e84dd22b7f3f8458a78b56
ChengBang Zhou <zhouchengbang@gmail.com>
2018-04-03 15:07:04 +0800

    decorator

--- higher_order_function.py
+++ higher_order_function.py
@@ -113,3 +113,69 @@ def f(x):
 f = lambda x: x*x
 print(f)
 print(f(5))
+
+# ========================== decorator =========================
+def now():
+    print('2015-01-03')
+
+f = now
+print(f)
+print(f.__name__)
+
+def log(func):
+    def wrapper(*args,**kw):
+        print('call %s():' % func.__name__)
+        return func(*args,**kw)
+    return wrapper
+
+@log
+def now():
+    print('2015-01-03')
+
+now()   # @log == (now = log(now))
+
+def log(text):
+    def decorator(func):
+        def wrapper(*args, **kw):
+            print('%s %s():' % (text, func.__name__))
+            return func(*args, **kw)
+        return wrapper
+    return decorator
+
+@log('execute')
+def now():
+    print('2015-01-03')
+
+now()
+print(now.__name__)
+
+import functools
+def log(func):
+    @functools.wraps(func)
+    def wrapper(*args,**kw):
+        print('call %s():' % func.__name__)
+        return func(*args,**kw)
+    return wrapper
+
+@log
+def now():
+    print('2015-01-03')
+
+now()
+print(now.__name__)
+
+def log(text):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kw):
+            print('%s %s():' % (text, func.__name__))
+            return func(*args, **kw)
+        return wrapper
+    return decorator
+
+@log('execute')
+def now():
+    print('2015-01-03')
+
+now()
+print(now.__name__)
