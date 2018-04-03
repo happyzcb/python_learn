63e8b34d1a54c4bdf9a9a0d82e2a8579a8bf8f2e
ChengBang Zhou <zhouchengbang@gmail.com>
2018-04-03 14:41:38 +0800

    filter

--- higher_order_function.py
+++ higher_order_function.py
@@ -43,3 +43,14 @@ def str2int_2(s):
     return reduce(lambda x, y: x * 10 + y, map(char2num, s))
 
 # =================== filter ===================
+def is_odd(n):
+    return n % 2 == 1
+
+print(list(filter(is_odd,[1,2,3,4,5,6,7,8])))
+
+def not_empty(s):
+    return s and s.strip()
+
+print(list(filter(not_empty,['a','b','','d',None,' '])))
+
+# ==================== sorted =================
