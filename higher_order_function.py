6f765ebfd9a81d75474e62c8008d494b924ec1a4
ChengBang Zhou <zhouchengbang@gmail.com>
2018-04-03 14:55:00 +0800

    function as return value

--- higher_order_function.py
+++ higher_order_function.py
@@ -59,3 +59,51 @@ print(sorted([1,6,3,5,9,2,1,-4],key=abs))
 print(sorted(['bob', 'about', 'Zoo', 'Credit']))
 print(sorted(['bob', 'about', 'Zoo', 'Credit'],key=str.lower))
 print(sorted(['bob', 'about', 'Zoo', 'Credit'],key=str.lower,reverse=True))
+
+# ==================== return function ===========
+def calc_sum(*args):
+    ax = 0
+    for n in args:
+        ax = ax + n
+    return ax
+
+def lazy_sum(*args):
+    def sum():
+        ax = 0
+        for n in args:
+            ax = ax + n
+        return ax
+    return sum
+
+f = lazy_sum(1, 3, 5, 7, 9)
+print(f)
+print(f())
+f1 = lazy_sum(1, 3, 5, 7, 9)
+f2 = lazy_sum(1, 3, 5, 7, 9)
+print(f1 == f2)
+
+# ======== 闭包 ===========
+def count():
+    fs = []
+    for i in range(1, 4):
+        def f():
+             return i*i
+        fs.append(f)
+    return fs
+
+f1, f2, f3 = count()
+print(f1(),f2(),f3())  #返回闭包时牢记一点：返回函数不要引用任何循环变量，或者后续会发生变化的变量
+
+def count2():
+    def f(j):
+        def g():
+            return j*j
+        return g
+    fs = []
+    for i in range(1, 4):
+        fs.append(f(i)) # f(i)立刻被执行，因此i的当前值被传入f()
+    return fs
+
+f1, f2, f3 = count2()
+print(f1(),f2(),f3())
+
