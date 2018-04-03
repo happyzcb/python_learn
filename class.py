8a616cd365aee2d5046cc3ef1db81f8601245c58
ChengBang Zhou <zhouchengbang@gmail.com>
2018-04-03 17:01:35 +0800

    getattr,setattr,hasattr

--- class.py
+++ class.py
@@ -22,3 +22,22 @@ class Student(object):
 
 
 bart = Student('Bart Simpson', 59)
+
+# get properties and methods
+print(dir('ABC'))
+
+# ==========  getattr(), setattr(), hasattr() ==============
+class Myobject(object):
+    def __init__(self):
+        self.x=9
+    def power(self):
+        return self.x * self.x
+
+obj = Myobject()
+
+print(hasattr(obj,'x'))  # 有属性'x'吗？
+print(hasattr(obj,'y'))  # 有属性‘Y’吗？
+setattr(obj,'y',19)  # 设置属性y
+print(hasattr(obj,'y'))  # 有属性‘Y’吗？
+print(getattr(obj,'y'))  # 有属性‘Y’吗？
+
