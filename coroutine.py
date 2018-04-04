a4f75512806543a2063554f304dd854fefe47cd7
ChengBang Zhou <zhouchengbang@gmail.com>
2018-04-04 15:18:48 +0800

    async/await,aiohttp

--- coroutine.py
+++ coroutine.py
@@ -63,7 +63,41 @@ def wget(host):
         # Ignore the body, close the socket
     writer.close()
 
+# loop = asyncio.get_event_loop()
+# tasks = [wget(host) for host in ['www.sina.com.cn', 'www.sohu.com', 'www.163.com']]
+# loop.run_until_complete(asyncio.wait(tasks))
+# loop.close()
+
+#====================== async/await =======================
+async def hello3():
+    print("Hello world!")
+    r = await asyncio.sleep(1)
+    print("Hello again!")
+
+# loop = asyncio.get_event_loop()
+# tasks = [hello3(),hello3()]
+# loop.run_until_complete(asyncio.wait(tasks))
+# loop.close()
+
+from aiohttp import web
+
+async def index(request):
+    await asyncio.sleep(0.5)
+    return web.Response(body=b'<h1>Index</h1>')
+
+async def hello(request):
+    await asyncio.sleep(0.5)
+    text = '<h1>hello, %s!</h1>' % request.match_info['name']
+    return web.Response(body=text.encode('utf-8'))
+
+async def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET', '/', index)
+    app.router.add_route('GET', '/hello/{name}', hello)
+    srv = await loop.create_server(app.make_handler(), '127.0.0.1', 8000)
+    print('Server started at http://127.0.0.1:8000...')
+    return srv
+
 loop = asyncio.get_event_loop()
-tasks = [wget(host) for host in ['www.sina.com.cn', 'www.sohu.com', 'www.163.com']]
-loop.run_until_complete(asyncio.wait(tasks))
-loop.close()
+loop.run_until_complete(init(loop))
+loop.run_forever()
